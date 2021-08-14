e29dbd4cd597c98540c7c320b2abc738bc820dd6
developeranikhossin <mdmanunbiswas321@gmail.com>
2021-08-15 00:01:13 +0600

    Bemax web site update

--- js/active.js
+++ js/active.js
@@ -1,5 +1,7 @@
 $(document) .ready(function(){
 
+/*  slider area */
+
     $('.slider-active').owlCarousel({
         loop:true,
         nav:true,
@@ -18,4 +20,41 @@ $(document) .ready(function(){
 
 
 
+
+/* project area */
+
+    $('.project-active').owlCarousel({
+        loop:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+    
+
+
+/*  vider area */
+    
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+        // other options
+    });
+
+
+
+
+
+
+
+
+
 })
+
