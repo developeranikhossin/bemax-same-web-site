1fb84d28d36427561f4e4e48e63bfb660700a98e
developeranikhossin <mdmanunbiswas321@gmail.com>
2021-08-16 21:09:42 +0600

    bemax update

--- js/active.js
+++ js/active.js
@@ -49,6 +49,64 @@ $(document) .ready(function(){
     });
 
 
+/* Choose Us area */
+
+var owl = $('.chose-active');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
+
+/* blog area */
+
+$('.blog-active').owlCarousel({
+    loop:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+/*   brand area   */
+
+
+var owl = $('.brand-active');
+owl.owlCarousel({
+    items:6,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
 
 
 
